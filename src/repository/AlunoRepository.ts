aa27b8d7d1bee925031067a4ddd9e847eee3608b
MizunoYudi <joaomizunovy@gmail.com>
2025-07-20 17:15:00 -0300

    Criação de Aluno em Repository

---
+++ src/repository/AlunoRepository.ts
@@ -0,0 +1,88 @@
+import { executarSQL } from "../database/mysql";
+import { AlunoUpdateDto } from "../model/dto/AlunoUpdateDto";
+import { AlunoEntity } from "../model/entity/AlunoEntity";
+
+export class AlunoRepository {
+    private static instance: AlunoRepository;
+
+    public static getInstance(): AlunoRepository {
+        if (!this.instance) {
+            this.instance = new AlunoRepository();
+        }
+        return this.instance
+    }
+
+    constructor() {
+        this.criarTabela();
+    }
+
+    private async criarTabela(): Promise<void> {
+        const query = `
+            CREATE TABLE IF NOT EXISTS cursosonline.Aluno (
+                id INT AUTO_INCREMENT PRIMARY KEY,
+                nome VARCHAR(80) NOT NULL,
+                email VARCHAR(80) NOT NULL,
+                cpf VARCHAR(11) NOT NULL
+            )
+        `;
+        try {
+            const resultado = await executarSQL(query, []);
+            console.log("Tabela Aluno criada: ", resultado);
+        } catch (error: any) {
+            console.log("Erro ao criar a tabela Usuario: ", error);
+        }
+    }
+
+    async inserirAluno(data: AlunoEntity) {
+        const query = `
+            insert into cursosonline.Aluno(nome, email, cpf)
+                values(?, ?, ?);
+        `;
+        const resultado = await executarSQL(query, [data.nome, data.email, data.cpf]);
+        console.log("Usuario inserido: ", resultado);
+
+        return new AlunoEntity(data.nome, data.email, data.cpf, resultado.insertId);
+    }
+
+    async buscarAlunoId(id: number){
+        const query = `
+            select * from cursosonline.Aluno where id = ?;
+        `;
+        const resultado = await executarSQL(query, []);
+        const aluno = resultado[0];
+        console.log("Aluno encontrado: ", aluno);
+        return new AlunoEntity(aluno.nome, aluno.email, aluno.cpf, aluno.id);
+    }
+
+    async buscarAlunos(id: number){
+        const query = `
+            select * from cursosonline.Aluno;
+        `
+        const resultado = await executarSQL(query, []);
+        console.log("Alunos encontrados: ", resultado);
+        return resultado;
+    }
+
+    async atualizarAluno(data: AlunoUpdateDto, id: number){
+        const query = `
+            update cursosonline.Aluno
+                set nome = ?,
+                set email = ?
+            where id = ?;
+        `
+        const resultado = await executarSQL(query, [data.nome, data.email, id]);
+        console.log("Usuario atualizado: ", resultado);
+        const aluno = resultado[0];
+        return this.buscarAlunoId(aluno.id);
+    }
+
+    async removerAluno(id: number){
+        const query = `
+            delete cursosonline.Aluno where id = ?;
+        `
+        const resultado = await executarSQL(query, [id]);
+        const usuario = this.buscarAlunoId(id);
+        console.log("Usuario excluido: ", usuario, "\nResultado: ", resultado);
+        return usuario;
+    }
+}
