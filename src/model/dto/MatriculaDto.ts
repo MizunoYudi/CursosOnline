9e5ed9380227a45891f18d962fa38a9a8d354695
MizunoYudi <joaomizunovy@gmail.com>
2025-07-21 18:20:35 -0300

    Atualização de Matricula Dto - Retirando campos desnecessários

--- src/model/dto/MatriculaDto.ts
+++ src/model/dto/MatriculaDto.ts
@@ -1,16 +1,12 @@
 export class MatriculaDto {
-    id: number;
     alunoId: number;
     cursoId: number;
 
     constructor(
         alunoId?: number,
         cursoId?: number,
-        dataMatricula?: Date,
-        id?: number
     ){
         this.alunoId = alunoId || 0;
         this.cursoId = cursoId || 0;
-        this.id = id || 0;
     }
 }
